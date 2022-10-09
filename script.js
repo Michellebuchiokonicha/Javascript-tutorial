f6ec17d56cf2684b8bc81fb95983e4f8d51d8cae
michellebuchiokonicha <michelleokonicha.com>
2022-10-09 16:55:55 +0100

    javascript tutorial

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function
+mimi(){
+    alert('Name of your school, course of study')
+    document.getElementById("Write a paragraph about you here").innerHTML = 'paragraph'
+   document.getElementById('giveitanId').style.background= 'choose color'
+   document.getElementById("gititanId").style.color = 'choose color'
+   console.log('your location')
+
+    //  console.log('I live in lagos')
+    //  alert('my name is mim')
+    // document.getElementById('mimi').style.color = 'red'
+    // document.getElementById('mimi').style.background = 'black'
+    // document.getElementById('mimi').innerHTML = 'I am fine'
+
+
+    // document.getElementById('pot').style.color = 'pink'
+
+}
