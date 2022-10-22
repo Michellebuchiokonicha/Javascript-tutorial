5d7911d61c642905e18a5fccc11a5feb0d04b9b2
michellebuchiokonicha <michelleokonicha.com>
2022-10-22 20:54:21 +0100

    javascript datatypes and operators

--- script.js
+++ script.js
@@ -1,19 +1,91 @@
 
 function
 mimi(){
-    alert('Name of your school, course of study')
-    document.getElementById("Write a paragraph about you here").innerHTML = 'paragraph'
-   document.getElementById('giveitanId').style.background= 'choose color'
-   document.getElementById("gititanId").style.color = 'choose color'
-   console.log('your location')
+//     alert('Name of your school, course of study')
+//     document.getElementById("Write a paragraph about you here").innerHTML = 'paragraph'
+//    document.getElementById('giveitanId').style.background= 'choose color'
+//    document.getElementById("gititanId").style.color = 'choose color'
+//    console.log('your location')
+   
 
     //  console.log('I live in lagos')
-    //  alert('my name is mim')
+    // //  alert('my name is mim')
+    // prompt('yes yes')
     // document.getElementById('mimi').style.color = 'red'
-    // document.getElementById('mimi').style.background = 'black'
-    // document.getElementById('mimi').innerHTML = 'I am fine'
+    // // document.getElementById('mimi').style.background = 'black'
+    document.getElementById('mimi').innerHTML = new Date()
 
+    // let x = 12
+    // var y = 10
+    // const  z = 13
+    // // document.write(x + y)
+    // var y = 16
 
+    // x = 14
+    // document.write(y)
+
+    // x = 14
+    // document.write(x)
+
+    // var
+    // const
+    // let 
     // document.getElementById('pot').style.color = 'pink'
 
+
+    // primitive and non-primitive datatypes
+    // primitive datatypes include: Number, string, undefined, symbol,NULL
+    // non-primitive datatypes are: objects and arrays
+
+    // string: '4 yes, 5'
+    // number: 5
+    // undefined: let x;
+    // NULL
+    // symbol:$@$
+
+    // array:[4, 'yes', 5]
+    // object:{1: '', 2: 'ope', ope: ''}
+
+
+    // operators
+    // +,-,*,/,%   4%2 == 0
+    // =,==,===, !=, !==
+
+    // = assignment operators
+    // ==, <,> comparison operators
+    // === strict comparison  operaators
+
+    // +,- ,* arithmetic operators -   'let x = 5+ 5
+    // :, ? conditional operators true or false, yes or no
+    // &&, ||, logical operators
+
+    // let 6 = 7;
+    // let 6 ==7;
+
+    // 5 == 5,
+    // 5 =='5'
+    // 5 === '5'
+
+    // if(5==5 || 5 == 6){
+        // return 5
+    // } else return 6
+
+    // 5==5 ? 5 : 6
+
+    // &&:and
+    // ||: or
+
+    // if(5 < 6 || 5 == 6){
+        // return 5
+    // }
+    // if(5 <= 6){ return 5}
+    // if(5>=6){return 5}
+    // if(5 !=6){return 'wrong value}
+    // if(5 !== 6){return "wrong value"}
+
+    //  5 >= 5 ? 6 : 8
+
+    // if(5 >= 5){return 6} else return 8
+
+    // 
 }
